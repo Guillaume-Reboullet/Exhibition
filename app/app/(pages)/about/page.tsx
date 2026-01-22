14aeee8aa27cf1ce1b2979ba7208039b2f6093cd
guillaume <guillaume@henri8.com>
2026-01-22 16:58:42 +0100

    Refactor: Enhance About page layout and styling for improved responsiveness and visual appeal

--- app/app/(pages)/about/page.tsx
+++ app/app/(pages)/about/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/app/_components/Header";
-import Navigation from "@/app/_components/Navigation - Pages";
+import Image from "next/image";
 import Contact from "@/app/_components/Contact";
 import Back from "@/app/_components/Back";
 
@@ -9,28 +9,43 @@ export default function About() {
       <Header title="À propos" subtitle="Guillaume Reboullet" />
 
       <div className="flex flex-col gap-(--spacing-section) lg:max-w-250 align-self-center">
-        <div className="flex flex-col gap-(--spacing-related)">
-          <h3>Qui suis-je ?</h3>
-          <p className="small-text font-serif">Étudiant en 3ᵉ année en école d’informatique à Epitech, je m’intéresse aux projets qui ont du sens. J’ai développé un intérêt particulier pour le domaine de la santé, où j’ai pu contribuer à des projets sérieux et humains.</p>
-        </div>
-        <div className="flex flex-col gap-(--spacing-related)">
-          <h3>Mes valeurs</h3>
-          <div className="flex flex-col gap-(--spacing-related) lg:flex-row lg:gap-(--spacing-default) lg:justify-between">
-            <div className="flex flex-col gap-(--spacing-micro) lg:max-w-75">
-              <h6 className="medium-text">Qualité</h6>
-              <p className="small-text font-serif">Je privilégie des solutions fiables et maintenables, même si cela demande plus de réflexion en amont.</p>
+        <div className="flex flex-col gap-(--spacing-related) items-center w-full sm:flex-row sm:items-center sm:justify-center sm:gap-(--spacing-default)">
+          <Image
+            src="/photo_profile.jpeg"
+            alt="Portrait de Guillaume Reboullet"
+            width={125}
+            height={125}
+            className="rounded-md lg:w-120"
+          />
+          <div className="flex flex-col items-between gap-(--spacing-default)">
+            <div className="flex flex-col gap-(--spacing-micro)">
+              <h3>Qui suis-je ?</h3>
+              <p className="small-text font-serif">Étudiant en 3ᵉ année en école d’informatique à Epitech, je m’intéresse aux projets qui ont du sens. J’ai développé un intérêt particulier pour le domaine de la santé, où j’ai pu contribuer à des projets sérieux et humains.</p>
             </div>
-            <div className="flex flex-col gap-(--spacing-micro) lg:max-w-75">
-              <h6 className="medium-text">Responsabilité</h6>
-              <p className="small-text font-serif">Ayant travaillé sur des projets en santé, je suis attentif à l’impact réel de ce que je développe.</p>
-            </div>
-            <div className="flex flex-col gap-(--spacing-micro) lg:max-w-75">
-              <h6 className="medium-text">Travail d’équipe</h6>
-              <p className="small-text font-serif">La réussite d’un projet repose sur la collaboration et la complémentarité des compétences au sein d’une équipe.</p>
+            <div className="hidden lg:flex flex-col gap-(--spacing-micro)">
+              <h3>Ce que je recherche</h3>
+              <p className="small-text font-serif">Je recherche une alternance en développement web ou mobile afin de continuer à progresser techniquement sur des projets exigeants, utiles et à fort impact, pouvant s’étendre jusqu’en 2028.</p>
             </div>
           </div>
         </div>
         <div className="flex flex-col gap-(--spacing-related)">
+          <h3>Mes valeurs</h3>
+          <div className="flex flex-col gap-(--spacing-related) lg:flex-row lg:gap-(--spacing-default) lg:justify-between">
+            <div className="flex flex-col gap-(--spacing-micro) lg:max-w-75 border border-dusk/20 rounded-lg p-(--spacing-related) group transition-all hover:shadow-lg">
+              <h6 className="medium-text">Qualité</h6>
+              <p className="small-text font-serif">Je privilégie des solutions fiables et maintenables, même si cela demande plus de réflexion en amont.</p>
+            </div>
+            <div className="flex flex-col gap-(--spacing-micro) lg:max-w-75 border border-dusk/20 rounded-lg p-(--spacing-related) group transition-all hover:shadow-lg">
+              <h6 className="medium-text">Responsabilité</h6>
+              <p className="small-text font-serif">Ayant travaillé sur des projets en santé, je suis attentif à l'impact réel de ce que je développe.</p>
+            </div>
+            <div className="flex flex-col gap-(--spacing-micro) lg:max-w-75 border border-dusk/20 rounded-lg p-(--spacing-related) group transition-all hover:shadow-lg">
+              <h6 className="medium-text">Travail d'équipe</h6>
+              <p className="small-text font-serif">La réussite d'un projet repose sur la collaboration et la complémentarité des compétences au sein d'une équipe.</p>
+            </div>
+          </div>
+        </div>
+        <div className="lg:hidden flex flex-col gap-(--spacing-related)">
           <h3>Ce que je recherche</h3>
           <p className="small-text font-serif">Je recherche une alternance en développement web ou mobile afin de continuer à progresser techniquement sur des projets exigeants, utiles et à fort impact, pouvant s’étendre jusqu’en 2028.</p>
         </div>
