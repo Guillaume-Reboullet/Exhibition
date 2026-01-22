474907574ae44117045ce49db1c40d9bbce26601
guillaume <guillaume@henri8.com>
2026-01-22 17:27:13 +0100

    Add: Create ExperienceCard component for displaying professional experiences with expandable details

--- app/app/(pages)/experiences/page.tsx
+++ app/app/(pages)/experiences/page.tsx
@@ -1,44 +1,53 @@
 import Back from "@/app/_components/Back";
 import Contact from "@/app/_components/Contact";
 import Header from "@/app/_components/Header";
+import ExperienceCard from "@/app/_components/ExperienceCard";
 
 export default function Experiences() {
   return (
     <>
       <Header title="Expériences" subtitle="Henri 8 x Epitech" />
 
-
       <div className="flex flex-col gap-(--spacing-section) lg:max-w-250 align-self-center">
-        {/* Expérience 1 */}
+
+        {/* Expérience Professionnelle */}
         <div className="flex flex-col gap-(--spacing-default)">
-          <div className="flex flex-col gap-(--spacing-micro)">
-            <h3>Henri 8</h3>
-            <div className="flex flex-col">
-              <span className="small-text text-lavender!">Alternance, Paris · 2024 – 2025</span>
-              <span className="small-text text-prussian">Développeur Web & Mobile Full Stack</span>
-            </div>
-          </div>
-          <div className="flex flex-col gap-(--spacing-related)">
-            <p className="small-text font-serif">Agence spécialisée dans la communication médicale, les applications santé et les événements scientifiques.</p>
-            <p className="small-text font-serif">Chez Henri 8, j’ai participé au développement et à la maintenance d’applications web et mobiles en production, dans un contexte médical exigeant. J’ai contribué à la stabilisation d’une application critique, avec la correction d’environ 50 bugs, et à des déploiements complets (API, bases de données, front-end). Certaines de ces applications étaient utilisées par 200 utilisateurs simultanés lors d’événements live, impliquant un fort niveau de fiabilité et de support en production.</p>
-          </div>
+          <h3>Professionnel</h3>
+          <ExperienceCard
+            title="Freelance"
+            location="Paris"
+            date="Octobre 2025 – Aujourd'hui"
+            role="Développeur Web & Mobile Full Stack"
+            description="En parallèle de mes études, j'exerce une activité de développeur freelance auprès de PME et de particuliers. J'interviens sur des missions de gestion de projet, de contrôle qualité (QA), de maintenance de sites web ainsi que sur des travaux d'extraction et de classification de données à la suite de campagnes d'emailing. Intégré à des projets déjà établis, j'ai assuré un rôle de conseil technique et fonctionnel auprès des clients, en veillant au respect des délais, à la qualité du produit livré et à la fiabilité des solutions mises en production."
+          />
+          <ExperienceCard
+            title="Henri 8"
+            location="Paris"
+            date="2024 – 2025"
+            role="Développeur Web & Mobile Full Stack"
+            description="Chez Henri 8, j'ai participé au développement et à la maintenance d'applications web et mobiles en production, dans un contexte médical exigeant. J'ai contribué à la stabilisation d'une application critique, avec la correction d'environ 50 bugs, et à des déploiements complets (API, bases de données, front-end). Certaines de ces applications étaient utilisées par 200 utilisateurs simultanés lors d'événements live, impliquant un fort niveau de fiabilité et de support en production."
+          />
         </div>
 
-        {/* Expérience 2 */}
+        {/* Formation */}
         <div className="flex flex-col gap-(--spacing-default)">
-          <div className="flex flex-col gap-(--spacing-micro)">
-            <h3>Epitech</h3>
-            <div className="flex flex-col">
-              <span className="small-text text-lavender!">Web@cademy - MSc, Paris · 2023 – 2028</span>
-              <span className="small-text text-prussian">Développeur Web & Mobile Full Stack</span>
-            </div>
-          </div>
-          <div className="flex flex-col gap-(--spacing-related)">
-            <p className="small-text font-serif">Formation reposant sur une approche par projets, axée sur l’échange, l’autonomie et la proximité avec le monde professionnel.</p>
-            <p className="small-text font-serif">À la Web Academy, j’ai évolué sur des projets menés en solo, en duo ou en groupe, dans un cadre exigeant avec des contraintes réelles, des deadlines strictes et des soutenances régulières. Cette expérience m’a appris à structurer une idée ou une vision pour la transformer en projet cohérent, organisé et exploitable, tout en développant une forte autonomie et une approche polyvalente du développement. </p>
-            <p className="small-text font-serif">À Epitech, le programme Pré-MSc m’a confronté à une approche beaucoup plus technique et exigeante du développement. Cette formation m’a demandé une grande rigueur dans ma manière de coder et d’aborder un problème, en privilégiant la compréhension, la structuration et la qualité des solutions plutôt que leur simple fonctionnement. Elle m’a permis de renforcer une méthode de travail précise et réfléchie, que j’applique aujourd’hui dans mes projets.</p>
-          </div>
+          <h3>Formation</h3>
+          <ExperienceCard
+            title="Epitech"
+            location="Paris"
+            date="2025 – 2028"
+            role="Master of Science"
+            description="À Epitech, le programme Pré-MSc m'a confronté à une approche beaucoup plus technique et exigeante du développement. Cette formation m'a demandé une grande rigueur dans ma manière de coder et d'aborder un problème, en privilégiant la compréhension, la structuration et la qualité des solutions plutôt que leur simple fonctionnement. Elle m'a permis de renforcer une méthode de travail précise et réfléchie, que j'applique aujourd'hui dans mes projets."
+          />
+          <ExperienceCard
+            title="Epitech"
+            location="Paris"
+            date="2023 – 2025"
+            role="Web@cademy"
+            description="À la Web Academy, j'ai évolué sur des projets menés en solo, en duo ou en groupe, dans un cadre exigeant avec des contraintes réelles, des deadlines strictes et des soutenances régulières. Cette expérience m'a appris à structurer une idée ou une vision pour la transformer en projet cohérent, organisé et exploitable, tout en développant une forte autonomie et une approche polyvalente du développement."
+          />
         </div>
+
       </div>
 
       <Contact
---
+++ app/app/_components/ExperienceCard/index.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+
+interface ExperienceCardProps {
+  title: string;
+  location: string;
+  date: string;
+  role: string;
+  description: string;
+  expandable?: boolean;
+}
+
+export default function ExperienceCard({
+  title,
+  location,
+  date,
+  role,
+  description,
+}: ExperienceCardProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div
+      className={"flex flex-col gap-(--spacing-related) border border-dusk/20 rounded-lg p-(--spacing-related) group transition-all hover:shadow-lg cursor-pointer"}
+      onClick={() => setIsExpanded(!isExpanded)}
+    >
+      <div className="flex flex-col">
+        <span className="medium-text text-prussian!">
+          {title} <span className='hidden sm:inline'>,</span>&nbsp;<span className="small-text italic block sm:inline">{location} · {date}</span>
+        </span>
+        <span className="small-text text-lavender!">{role}</span>
+      </div>
+
+      {!isExpanded ? (
+        <p className={`small-text font-serif transition-all ${isExpanded ? '' : 'line-clamp-2'}`}>
+          {description}
+        </p>
+      ) : (
+        <p className="small-text font-serif">{description}</p>
+      )}
+
+      <div className="flex w-full justify-center">
+        <Image
+          src="/arrow_right.svg"
+          alt="open/close button"
+          width={36}
+          height={36}
+          className={`transition-transform ${isExpanded ? 'rotate-270' : 'rotate-90'}`}
+        />
+      </div>
+    </div>
+  );
+}
